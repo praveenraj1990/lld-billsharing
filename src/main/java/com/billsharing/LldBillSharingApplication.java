e856dad57464235ec1a8716a76c150f812fceca1
praveen-raj <praveen.raj@mettl.com>
2022-01-22 19:14:17 +0530

    Initial commit

---
+++ src/main/java/com/billsharing/LldBillSharingApplication.java
@@ -0,0 +1,13 @@
+package com.billsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LldBillSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LldBillSharingApplication.class, args);
+    }
+
+}
